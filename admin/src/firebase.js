43368dd1f577afa5f3cb290f30d08b690f70fa43
KOTa2k4 <161137741+KOTa2k4@users.noreply.github.com>
2025-06-24 22:29:50 +0700

    Update firebase.js

--- admin/src/firebase.js
+++ admin/src/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCwctu39z7Gc5-C-GXYFGp2SSdF7tpTK60",
-    authDomain: "neflix1-2cf45.firebaseapp.com",
-    projectId: "neflix1-2cf45",
-    storageBucket: "neflix1-2cf45.firebasestorage.app",
-    messagingSenderId: "402799684358",
-    appId: "1:402799684358:web:04ca91d861955061e08c87",
-    measurementId: "G-2FJRX91S3R"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 };
 
 const firebase = initializeApp(firebaseConfig)
